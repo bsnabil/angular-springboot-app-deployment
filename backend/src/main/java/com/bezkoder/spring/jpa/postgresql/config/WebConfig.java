f43c47a064b933e5bd03857d3328541ae4250ed1
vagrant <vagrant@master-node>
2024-12-09 10:45:19 +0000

    push project

---
+++ backend/src/main/java/com/bezkoder/spring/jpa/postgresql/config/WebConfig.java
@@ -0,0 +1,20 @@
+
+package com.bezkoder.spring.jpa.postgresql.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")  // Allow all endpoints
+                .allowedOrigins("http://localhost:8081")  // Allow the Angular app
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // Allowed HTTP methods
+                .allowedHeaders("*")  // Allow all headers
+                .allowCredentials(true);  // Allow credentials (cookies, authorization headers, etc.)
+    }
+}
+
